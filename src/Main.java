54adf853c50cdbf4bc2ddea0884ff0f90e6647f1
mikju <s224279@student.dtu.dk>
2022-09-19 16:23:36 +0200

    First commit, questions and answers added to main project.

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Terningespil for IOOuterActive.
+Vi stiller disse spørgsmål til kunden og projektlederen..
+
+Spørgsmål til kunden
+Går turen på skift?
+    Ja mellem spiller 1 og 2.
+Kan man blive ved med at slå dobbelt og få ekstra ture?
+    Ja
+Gælder det, at man vinder hvis man slår to seksere i en runde, slår ikke dobbelt og runden efter slår to seksere igen? Eller skal det forstås i speciering 3, at man aldrig kan slå dobbelt efter sidste runde og kun efter sidste kast.
+    Ekstra kast kun!
+Vinder man hvis man når 40 point præcist selvom man ikke slog dobbelt?
+    Man skal slå dobbelt efter man har opnået 40 point.
+Hvis man får mere end 40 point, kan man så stadig vinde spillet?
+    Ja, hvis man slår dobbelt.
+Hvordan ønskes det at det bliver indikeret at en spiller har vundet.
+    Print en besked om at spiller 1 eller spiller 2 har vundet, slut spillet eller spørg om man vil spille igen.
+Hvad er et almindelig menneske i vores målgruppe? evt. PEG-rating
+    Folk der ikke kan kode, men har arme, øjne og kan læse.
+Hvis man slår dobbelt et, efter at være nået over 40 point, vinder man så, eller mister man stadig alle sine point.
+    Man mister alle sine point og skal starte forfra.
+
+Spørgsmål til Projektleder
+Er der en compiler installeret på maskinerne?
+I Så Fald Hvilken compiler?
+    Java 17 er installeret
+Hvis ikke, har vi mulighed for at installere en?
+    Se ovenstående.
+Hvilke input devices er der? Keyboard, mus?
+    Keyboard og mus.
+Hvilke output devices? Er der skærm tilgengængelig og hvad er størrelsen/opløsningen?
+    1080p, ja der er skærm.
+Hvad beskriver “virker korrekt”, når det gælder test af 1000 kast.
+    Indenfor 5% af variation af normalfordelingen.
+
+For info: hvis projektleder ikke kan svare, må du spørge kunden.
+
+         */
+    }
+}
