66f0e766cbbcb1edf858b90cfc079c5499f3a162
mikju <s224279@student.dtu.dk>
2022-09-19 17:04:17 +0200

    Test of main.java, testing methods of random dice roll within 5% of normal distribution.

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        /* her køres testen for om kravspecificeringen om at bægeret skal virke korrekt, som er specificeret
+        til at være inden for 5% af normalfordelingen på 1000 kast.
+        */
+    }
+}
