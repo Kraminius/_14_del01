d8174600d110d0f52d7526783b81292409861898
Toby <tobiasgoerlyk@hotmail.com>
2022-09-26 16:55:02 +0200

    Added SumOfDice class To calculate a sum of rolls.

--- src/SumOfDice.java
+++ src/SumOfDice.java
@@ -1,7 +1,7 @@
 public class SumOfDice {
     private int sum;
 
-    public void calcSum(RollDice rollDice){
+    public void calcSum(RollDice rollDice){ //Calculates sum of int[]
         sum = 0;
         for(int i = 0; i < rollDice.getOurRolls().length; i++){
             sum += rollDice.getOurRolls()[i];
