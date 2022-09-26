49f37aabac55f170815c080b7ad01484d7c71f53
AWG96 <s221094@student.dtu.dk>
2022-09-26 16:39:21 +0200

    created RollDice to roll a dice

--- src/RollDice.java
+++ src/RollDice.java
@@ -1,3 +1,26 @@
+import java.util.Random;
+
 public class RollDice {
 
+    private int []ourRolls;
+
+    private final int numberOfRolls = 2;
+    private Die die = new Die();
+    public void Roll(){
+        ourRolls = new int[numberOfRolls];
+        Random random = new Random();
+
+        for (int i = 0;i<numberOfRolls;i++){
+            ourRolls[i] = random.nextInt((die.getMaximum()- die.getMinimum())+1)+1;
+
+        }
+    }
+
+    public int[] getOurRolls() {
+        return ourRolls;
+    }
+
+    public void setOurRolls(int[] ourRolls) {
+        this.ourRolls = ourRolls;
+    }
 }
