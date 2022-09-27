bc3bb9855682c8976f8f5281ac3dabe2557d73f1
Your Name <you@example.com>
2022-09-27 15:09:18 +0200

    Comments in display class

--- src/Display.java
+++ src/Display.java
@@ -1,2 +1,7 @@
 public class Display {
+
+    //Efter input fra kunden, vil vi sætte die1 og die2 på hver sin linjen
+    //Efter input fra kunden, skal der ønskes tillykke hver gang man slår dobbelt
+
+
 }
