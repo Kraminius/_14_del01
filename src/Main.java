d1e0fb95cafe8358d1c43ea9f8b9b548f8e3e4db
Toby <tobiasgoerlyk@hotmail.com>
2022-09-28 13:29:41 +0200

    Made some comments to Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-
-        Game game = new Game(2);
-
+        Game game = new Game(2); //Creates new instance of a game
     }
 }
