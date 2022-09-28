d66a0cce725c4e766a4fe1b8f6aab08f666abd9e
Toby <tobiasgoerlyk@hotmail.com>
2022-09-28 13:39:54 +0200

    Made some comments to Players

--- src/Players.java
+++ src/Players.java
@@ -1,33 +1,15 @@
 public class Players {
-
-    private Player[] allPlayers;
-
-
-    public Players(){
-
-    }
-
+    private Player[] allPlayers; //Creates a Player array to hold all the players.
     public Players(int numberOfPlayers){
+        allPlayers = new Player[numberOfPlayers]; //allPlayers is equal to a new instance of Player array for each time the method is called.
 
-
-        allPlayers = new Player[numberOfPlayers];
-
-        for(int i =0; i<numberOfPlayers; i++){
-
-            allPlayers[i] = new Player(i+1);
-
-
-
+        for(int i =0; i<numberOfPlayers; i++){ //For loop repeats a number of times equal to the amount of players
+            allPlayers[i] = new Player(i+1); //Adds a player to the array, that is a new instance of Player with a playerNumber equal i+1.
         }
-
-
-
     }
-
     public Player[] getAllPlayers() {
         return allPlayers;
-    }
-
+    } //Getter and Setter(Below) for the Player array
     public void setAllPlayers(Player[] allPlayers) {
         this.allPlayers = allPlayers;
     }
